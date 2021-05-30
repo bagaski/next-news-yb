4b62925b920df65eac5b8ddb95bcd7ba77099ea0
Yiorgos Bagakis <ybagakis@gmail.com>
2021-05-30 10:10:05 +0200

    nextcongig js for  210530

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
-const basePath = process.env.NODE_ENV === 'production' ? '/next-news-yb' : '';
+const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
-module.exports = {
-    basePath,
-    assetPrefix: `${basePath}/`
-};
+const withNextOptimizedImages = require('next-optimized-images');
+
+const withMDX = require('@next/mdx')({
+    extension: /\.mdx?$/,
+})
+
+module.exports = withNextOptimizedImages(
+    withMDX({
+        webpack: (config, { isServer }) => {
+            if (!isServer) {
+                config.node = {
+                    fs: 'empty'
+                }
+            }
+            return config
+        },
+        pageExtensions: ['js', 'jsx', 'mdx'],
+        target: 'serverless',
+        basePath: ghPages ? '/blog/' : '',
+        assetPrefix: ghPages ? '/blog/' : '',
+    })
+)
